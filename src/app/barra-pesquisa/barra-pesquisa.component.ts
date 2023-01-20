041c17dbc52fddcc64d02332d3e7114a0ad7c8d3
Mateus <mateusmlopestr@gmail.com>
2023-01-20 10:28:24 -0300

    build

---
+++ src/app/barra-pesquisa/barra-pesquisa.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-barra-pesquisa',
+  templateUrl: './barra-pesquisa.component.html',
+  styleUrls: ['./barra-pesquisa.component.css']
+})
+export class BarraPesquisaComponent {
+  descricao = "";
+
+  constructor(
+    private router: Router
+  ) {}
+
+  pesquisar() {
+    console.log(this.descricao.length === 0);
+    if (!this.descricao) {
+      this.router.navigate([""]);
+      return;
+    }
+    this.router.navigate(["produtos"], {queryParams: { descricao: this.descricao }});
+  }
+
+}
